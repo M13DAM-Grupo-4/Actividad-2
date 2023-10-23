49e13bbdef898a926b7f388172b96de4cbcd240f
Isaac Cajiao Valle <icv2@outlook.es>
2023-10-24 01:07:39 +0200

    Reformateo.

--- app/src/main/java/m13dam/grupo4/actividad2/MainActivity.java
+++ app/src/main/java/m13dam/grupo4/actividad2/MainActivity.java
@@ -5,10 +5,6 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.os.StrictMode;
 
-import java.sql.Connection;
-import java.sql.SQLException;
-import java.sql.Statement;
-
 import m13dam.grupo4.actividad2.Database.DatabaseManager;
 
 public class MainActivity extends AppCompatActivity {
