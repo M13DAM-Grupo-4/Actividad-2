c81591928284c92229f11fe1d77e296e71503922
Llesua <jesusinvalle@gmail.com>
2023-10-28 00:22:46 +0200

    Login interafce

--- app/src/main/java/m13dam/grupo4/actividad2/ListasEmpleados.java
+++ app/src/main/java/m13dam/grupo4/actividad2/ListasEmpleados.java
@@ -2,17 +2,46 @@ package m13dam.grupo4.actividad2;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 import androidx.activity.OnBackPressedCallback;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import m13dam.grupo4.actividad2.Database.DatabaseManager;
+
 public class ListasEmpleados extends AppCompatActivity  {
+
+    private Button añadirEmple;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.principal);
+
+        añadirEmple = findViewById(R.id.añadir_empleado);
+
+        añadirEmple.setOnClickListener(new View.OnClickListener(){
+            public void onClick (View v) {
+                Intent intent = new Intent(ListasEmpleados.this, FormularioEmpleado.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+
+
+
+
+
+
+
+
+
+
 
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
